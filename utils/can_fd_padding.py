96896ea71299c2f8c7f3ac891d06e148e83a1cd3
Omolade <oikumapa@uccs.edu>
2023-02-04 22:35:06 -0700

    initial commit

---
+++ utils/can_fd_padding.py
@@ -0,0 +1,10 @@
+def can_fd(d):
+    return ( d if d <=  8 else
+        12 if 8 < d <= 12 else
+        16 if 12 < d <= 16 else
+        20 if 16 < d <= 20 else
+        24 if 20 < d <= 24 else
+        32 if 24 < d <= 32 else
+        48 if 32 < d <= 48 else
+        64)   
+    
